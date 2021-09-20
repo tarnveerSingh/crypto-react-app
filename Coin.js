d9e3c0c0c0e311d0e3a4bca5846966479ed3bbf0
Tarnveer Singh <66130223+tarnveerSingh@users.noreply.github.com>
2021-09-21 03:34:16 +1000

    Update Coin.js

--- Coin.js
+++ Coin.js
@@ -21,27 +21,53 @@ const Coin = ({
           <p className="coin-symbol">{symbol}</p>
         </div>
         <div className="coin-data">
-          <p className="coin-price">A${price}</p>
+          {price ? <p className="coin-price">{price && `A$${price}`}</p> : ''}
+
+          {/* {price && <p className="coin-price">{price && `A$${price}`}</p>} */}
+          {/* <p className="coin-price">{price && `A$${price}`}</p> */}
+
           {priceChange < 0 ? (
             <p className="coin-percent-black">
               {priceChange && priceChange.toFixed(2)}%
             </p>
           ) : (
             <p className="coin-percent-white">
-              {priceChange && priceChange.toFixed(2)}%
+              {priceChange && priceChange.toFixed(2)}
             </p>
           )}
-          {priceChange24 < 0 ? (
+          {/* priceChange24  if not required */}
+          {/* {priceChange24 < 0 ? (
+            <p className="coin-percent-black">
+              ${priceChange && priceChange24.toFixed(3)}
+            </p>
+          ) : (
+            <p className="coin-percent-white">
+              {priceChange24 && priceChange24}
+            </p>
+          )} */}
+          {/* difference $ */}
+          {/* {priceChange24 < 0 ? (
             <p className="coin-percent-black">
               ${priceChange && priceChange24.toFixed(2)}
             </p>
           ) : (
             <p className="coin-percent-white">
-              ${priceChange && priceChange24.toFixed(2)}
+              ${priceChange24 && priceChange24}
+              
+            </p>
+          )} */}
+
+          {priceChange24 < 0 ? (
+            <p className="coin-percent-black">
+              ${priceChange24 && priceChange24.toFixed(2)}
+            </p>
+          ) : (
+            <p className="coin-percent-white">
+              ${priceChange24 && priceChange24.toFixed(2)}
             </p>
           )}
-
           <p className="coin-volume">${volume.toLocaleString()}</p>
+          {/* <p className="coin-volume">${volume.toFixed(2)}</p> */}
 
           <p className="coin-mkp">Mkt Cap: ${mkp.toLocaleString()}</p>
         </div>
