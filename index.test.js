0195bb883e2eeb2893e3ebe1aef01a3c1ca8ab14
Tarnveer Singh <66130223+tarnveerSingh@users.noreply.github.com>
2021-09-24 02:26:06 +1000

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,6 +1,10 @@
 // eslint-disable-next-line no-undef
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+
+import { render, screen } from '@testing-library/react';
+import  from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
