130f64530350ca9768b1593ade1e78a2ae23f459
Tarnveer Singh <66130223+tarnveerSingh@users.noreply.github.com>
2021-10-05 14:08:28 +1100

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,10 +1,10 @@
-// eslint-disable-next-line no-undef
+// // eslint-disable-next-line no-undef
 
-import { render, screen } from '@testing-library/react';
-import IndexPage from './pages/IndexPage';
+// import { render, screen } from '@testing-library/react';
+// import IndexPage from './pages/IndexPage';
 
-test('renders learn react link', () => {
-  render(<Index />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<Index />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
